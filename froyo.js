82b6531b3c78973cc48dd2d798c39f431c188c3f
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2023-11-16 11:30:27 -0500

    updated

--- froyo.js
+++ froyo.js
@@ -1,4 +1,27 @@
-const userInputString = prompt(
+//I'm referencing the Unit.2.Stats.Starter for this 
+// Prompt the user for a list of integers separated by commas.
+let userInputString = prompt(
     "Please enter the ice cream flavors you'd like to order.",
     "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
   );
+
+// Split the string of numbers into an array of strings.
+let stringArray = userInputString.split(",")
+
+// Creating an object for the array of strings storing count of flavors.
+const flavorCounts = {};
+
+// Counting the occurrences of each flavor
+const numFlavors = [i]
+let i = " "
+for(const key of flavorCounts) {
+    if (flavorCounts[numFlavors] = flavorCounts[numFlavors])
+    flavorCounts=flavorCounts[numFlavors] + 1
+    else {
+        1;
+}
+console.log(flavorCounts[key])
+}
+
+console.table(flavorCounts);
+
